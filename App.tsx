75926e8ce903a92e9e4feb6cc537af3bbd44298b
Gabriel Wilson <gabrielvincentw@gmail.com>
2023-06-28 14:02:04 -0400

    Revert "Added react-native-splash-screen package"
    This reverts commit 4e71532841cbecdbb747a3a28f76db2ff5e0561d.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import MainScreen from "./screens/MainScreen";
 import { Provider } from "react-redux";
 import { store, persistedStore } from "./src/redux/store";
 import { PersistGate } from "redux-persist/integration/react";
-import SplashScreen from "react-native-splash-screen";
 
 export default function App() {
-    useEffect(() => {
-        SplashScreen.hide();
-    }, []);
-
     return (
         <Provider store={store}>
             <PersistGate persistor={persistedStore}>
